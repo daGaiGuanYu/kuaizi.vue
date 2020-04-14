a6989678047adbe1104684f5a4408985014d941b
关于 <372301467@qq.com>
2020-04-14 16:21:16 +0800

    update

---
+++ src/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="overflow-y: hidden" :style="{ height }">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['collapse'],
+  computed: {
+    height(){
+      return this.collapse?'0px':'auto'
+    }
+  },
+  mounted(){
+
+  }
+}
+</script>
