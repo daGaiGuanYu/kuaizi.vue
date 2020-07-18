f01c3406e0b154391c5feb72455c03f95d79ef3a
关于 <372301467@qq.com>
2020-07-18 11:50:42 +0800

    第一个组件 collapse-box

---
+++ test/cmp/collapse-box.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .demo
+    h1 collapse-box
+    .upper
+      span 这是 collapse-box 上面的东西
+      span.btn(@click="collapse=!collapse") {{collapse?'放':'收'}}
+      
+      collapse-box(:collapse="collapse")
+        .haha haha
+        .heihei heihei
+      
+      .underneath 这是 collapse-box 下面的东西
+</template>
+<script>
+import CollapseBox from '../../src/collapse-box/index.vue'
+
+export default {
+  name: 'collapse-box-demo',
+  components: {
+    CollapseBox
+  },
+  data(){
+    return {
+      collapse: false
+    }
+  }
+}
+</script>
---
+++ test/entry/app.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .root-root
+    collapse-box-demo
+</template>
+
+<script>
+import CollapseBoxDemo from '../cmp/collapse-box'
+
+export default {
+  components: {
+    CollapseBoxDemo
+  }  
+}
+</script>
+
+<style lang="stylus">
+body
+  line-height: 36px
+.btn
+  display: inline-block
+  width: 36px
+  text-align: center
+  margin: 10px
+  background: #000
+  color: #fff
+  border-radius: 18px
+  user-select: none
+  cursor: pointer
+</style>
